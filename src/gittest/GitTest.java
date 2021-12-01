b1eaffcf36fc2705316dd85c9d6304ed71270b0f
Sarah424 <90831286+Sarah424@users.noreply.github.com>
2021-12-01 08:58:47 +0300

    commit123

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -17,5 +17,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("MEOW");
+        System.out.println("Sara");
     }    
 }
